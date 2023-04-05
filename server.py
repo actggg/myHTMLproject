63ad7e10051f71ff780c9d386ed43696f4484b56
yur.raz@mail.ru <yur.raz@mail.ru>
2023-04-05 23:54:25 +0300

    sss

--- server.py
+++ server.py
@@ -14,8 +14,8 @@ login_manager.init_app(app)
 
 
 @app.route("/")
-def index():
-    return render_template("base.html", news=news)
+def base():
+    return render_template("test.html")
 
 
 
@@ -84,27 +84,14 @@ def logout():
     return redirect("/")
 
 
-@app.route('/news', methods=['GET', 'POST'])
-@login_required
-def add_news():
-    form = NewsForm()
-    if form.validate_on_submit():
-        db_sess = db_session.create_session()
-        news = News()
-        news.title = form.title.data
-        news.content = form.content.data
-        news.is_private = form.is_private.data
-        current_user.news.append(news)
-        db_sess.merge(current_user)
-        db_sess.commit()
-        return redirect('/')
-    return render_template('news.html', title='Добавление новости',
-                           form=form)
+@app.route('/catalog', methods=['GET', 'POST'])
+def catalog():
+    return render_template('catalog.html', title='Каталог')
 
 
 def main():
     db_session.global_init("db/blogs.db")
-    app.run(port=8100, host='127.0.0.1')
+    app.run(port=8112, host='127.0.0.1')
 
 
 if __name__ == '__main__':
